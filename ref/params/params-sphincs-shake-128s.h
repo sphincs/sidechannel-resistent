dc1a808d1f86736ef9d13d24aca81c3ac26d0e2b
sfluhrer <sfluhrer@cisco.com>
2024-03-26 11:51:28 -0400

    Add files via upload

--- ref/params/params-sphincs-shake-128s.h
+++ ref/params/params-sphincs-shake-128s.h
@@ -6,12 +6,12 @@
 /* Hash output length in bytes. */
 #define SPX_N 16
 /* Height of the hypertree. */
-#define SPX_FULL_HEIGHT 66
+#define SPX_FULL_HEIGHT 63
 /* Number of subtree layer. */
-#define SPX_D 22
+#define SPX_D 7
 /* FORS tree dimensions. */
-#define SPX_FORS_HEIGHT 6
-#define SPX_FORS_TREES 33
+#define SPX_FORS_HEIGHT 12
+#define SPX_FORS_TREES 14
 /* Winternitz parameter, */
 #define SPX_WOTS_W 16
 
--- ref/params/params-sphincs-shake-256s.h
+++ ref/params/params-sphincs-shake-256s.h
@@ -4,14 +4,14 @@
 #define SPX_NAMESPACE(s) SPX_##s
 
 /* Hash output length in bytes. */
-#define SPX_N 16
+#define SPX_N 32
 /* Height of the hypertree. */
-#define SPX_FULL_HEIGHT 66
+#define SPX_FULL_HEIGHT 64
 /* Number of subtree layer. */
-#define SPX_D 22
+#define SPX_D 8
 /* FORS tree dimensions. */
-#define SPX_FORS_HEIGHT 6
-#define SPX_FORS_TREES 33
+#define SPX_FORS_HEIGHT 14
+#define SPX_FORS_TREES 22
 /* Winternitz parameter, */
 #define SPX_WOTS_W 16
 
